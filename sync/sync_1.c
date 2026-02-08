52a80d8aeeea49ad414ef61a1a1f89a142bd8f6e
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-02-08 22:07:37 +0530

    Add files via upload

---
+++ sync/sync_1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<errno.h>
+
+int main()
+{
+    int fd = 0;
+    char Buffer[] = "Indiaismycountry";
+    int iRet = 0;
+
+    fd = open("Demo.txt",O_CREAT | O_WRONLY | O_APPEND , 0777);  // IMP
+
+    if(fd < 0)
+    {
+        printf("Unable to open the file\n");
+        printf("Reason : %s\n",strerror(errno));
+        return -1;
+    }
+
+    printf("File succesfully opened with fd : %d\n",fd);
+
+    iRet = write(fd , Buffer , 16);   // Overwrite issue
+
+    printf("%d bytes gets succesfully written\n",iRet);
+
+    close(fd);
+    
+    return 0;
+}
