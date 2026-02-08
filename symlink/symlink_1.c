83498f7c3058e2f9b7bd313e7b7881c1e966a37e
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-02-08 22:08:55 +0530

    Add files via upload

---
+++ symlink/symlink_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<errno.h>
+
+int main()
+{
+    int iRet = 0;
+
+    iRet = symlink("demox.txt","hello.txt");
+
+    if(iRet == 0)
+    {
+        printf("SymLink is successfull\n");
+    }
+    else
+    {
+        printf("%s\n ",strerror(errno));
+    }
+
+    return 0;
+}
