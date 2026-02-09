143b3b1f589e1f85480fa1c964e9d92f49542c5e
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-02-09 12:08:16 +0530

    Add files via upload

---
+++ pwrite/pwrite_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main()
+{
+    int fd = 0;
+    off_t offset = 0;
+    char Buffer[100] = "Pune";
+    int iRet = 0;
+
+    fd = open("./LSPX.txt",O_WRONLY);
+
+    iRet = pwrite(fd , Buffer , 4 , 10);
+
+    offset = lseek(fd , 0 , SEEK_CUR);
+
+    printf("Current offset is : %ld\n",offset);
+
+
+    return 0;
+}
